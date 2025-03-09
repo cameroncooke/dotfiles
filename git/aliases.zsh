d29d574f210967db07e0bfb9c6a271b8c47ec506
Cameron Cooke <web@cameroncooke.com>
2025-03-09 17:38:20 +0000

    WIP

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,7 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
-alias gu='git submodule update --init --recursive'
+# alias gu='git submodule update --init --recursive'
 alias gp='git pull origin && gu'
 
 #alias both-merged =	 !git-both-merged
