03dade62c3815c19bd27f0d577eea5c0087a0a76
Cameron Cooke <web@cameroncooke.com>
2025-06-11 11:16:52 +0100

    Update install.sh

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,3 +1,8 @@
 echo "  Installing NVM you."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+
+echo "   Installing Claude Code"
+
+npm install -g @anthropic-ai/claude-code
